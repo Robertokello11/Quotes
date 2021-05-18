f2f3ddb9bfa6a57242e50933ed3d39636242b38a
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-05-18 03:27:18 +0300

    make all added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { QuotesFormComponent } from './quotes-form/quotes-form.component';
-import { HighlightQuoteDirective } from './highlight-quote.directive';
 import { DatePipePipe } from './date-pipe.pipe';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteDetailsComponent,
+    DatePipePipe,
     QuotesFormComponent,
-    HighlightQuoteDirective,
-    DatePipePipe
+    HighlightQouteDirective,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
